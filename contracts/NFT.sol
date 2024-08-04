a97bc1cf9feb13dd2e60a6a503d38772aed6c1fd
Tpatop crypto <cisco.medvedev2000@gmail.com>
2024-08-04 07:11:12 +0000

    feat: initiated the project

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 contract TestNFT is ERC721, ERC721Burnable {
     constructor()
-        ERC721("tpatopnft","TNFT")
+        ERC721("tpatopnfts","tptnft")
     {}
 
     function safeMint(address to, uint256 tokenId) public {
